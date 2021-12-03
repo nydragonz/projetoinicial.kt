b347172c05154b15b170f1eafb3c1bb85fb5590b
nydragonz <95483210+nydragonz@users.noreply.github.com>
2021-12-03 10:22:39 -0300

    simples.kt
    qui tem o simples programa

---
+++ simples.kt
@@ -0,0 +1,6 @@
+fun main() {
+    greet("worldwideweb")
+    }
+    fun greet(name: String= "kotlin") {
+    println("a $name é a propria rede em si na internet em que vivemos, www significa $name")
+    }
